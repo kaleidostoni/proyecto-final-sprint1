60db0b49cbbd8470af7413c499db6f64de2cdfc8
kaleidostoni <kaleidostoni@gmail.com>
2017-10-31 12:11:39 +0000

    puliendo los comentarios

--- tarjeta-de-credito/js/app.js
+++ tarjeta-de-credito/js/app.js
@@ -1,33 +1,34 @@
 //revirtiendo el numero de tarjeta de credito
-var cardNumber = [7,2,5,4];
+var cardNumber = [7,2,5,4,];
 var reversedNumber =cardNumber.reverse();
-/* creando una funcion que separe los numeros que se encuentren en
- index non de los que se encuentran en index par (menores a 4)*/
+/* creando una funcion lunhAlgoritm que separe los numeros que se encuentren en
+ index non y los sume en la variable suma1*/
 
- /*declaramos la variable suma1 donde se sumaran los numeros que filtre
- el for*/
+/*dentro de la misma funcion lunhAlgoritm separamos con otro for los numeros que
+se encuentren en index par si son menores a 4, los multiplique por 2 y
+ los sume en la variable suma2*/
 
- // declaramos la var suma2 donde se suman los numeros que filtra for
- //sumamos los dos sumas en la variable totalSum
+ /*por último que la funcion retorne las suma de las dos sumas en la variable
+ totalSum*/
 
  var lunhAlgoritm = function(array){
-   var suma1 = 0;
+   var sum1 = 0;
    for (var i=0; i<array.length; i= i+2){
-     suma1+=array[i];
+     sum1+=array[i];
     }
-    var suma2 = 0;
+    var sum2 = 0;
     for (var j=1; j<array.length; j= j+2){
       if(array[j] < 4){
-        suma2+=array[j]*2;
+        sum2+=array[j]*2;
       }
-    }var totalSum = suma1 + suma2;
+    }var totalSum = sum1 + sum2;
   return totalSum;
   }
 //llamando a la función lunnAlgoritm
   lunhAlgoritm(reversedNumber);
 
-  /*haciendo una funcion  pairsOneDigit que reuna  con . push en un array
-   los numeros en index par mayores a 4*/
+  /*haciendo una funcion  pairsBiggerThanFourt que reuna  con . push en un array
+   los numeros en index par mayores a 4 y los multiplique por 2*/
 
   var pairsBiggerThanFour = function(array){
     var arrayNumbersFiltered = [];
@@ -41,13 +42,13 @@ var reversedNumber =cardNumber.reverse();
   pairsBiggerThanFour(reversedNumber);
 
 
-  /*creando una función que asigne un valor a cada numero del
-  arrayNumbersFiltered el numero que resulte de multiplicarlo por
-  dos y sumar su dos digitos para despues sumar esos resultados*/
+  /*creando una funcion  que usando un for vaya iterando cada valor de mi array
+  arrayNumbersFiltered y separe con .split sus dos cifras para poder sumarlas
+  y guardarlas en otro arreglo */
 
 
      var turningToOneDigit = function(array){
-        var suma4 = [];
+       var sum4 = [];
        for(var l = 0; l < array.length; l++){
          var separateDigits = array[l].toString().split('');
          console.log(separateDigits);
